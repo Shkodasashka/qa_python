10685003c32ccf4ba5c2bb13b68198700ae0f617
Sasha Shkoda <shkoda_sashka@mail.ru>
2025-09-28 00:23:57 +0300

    Создание тестов на формирование жанра книги, если книга есть в books_genre и её жанр входит в список genre

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+from main import BooksCollector
+
+@pytest.fixture
+def book():
+    book = BooksCollector()
+    return book
--- test_main.py
+++ test_main.py
@@ -18,7 +18,7 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(book) == 2
+        assert len(book.books_genre) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
@@ -28,11 +28,40 @@ class TestBooksCollector:
         book.add_new_book('Марсианин')
         book.add_new_book('Марсианин')
 
-        assert len(book) == 1
+        assert len(book.books_genre) == 1
 
-    def test_add_new_book_neggative_add_book_out_of_range(self, book):
+    def test_add_new_book_neggative_add_book_name_out_of_range(self, book):
 
         book.add_new_book('')
         book.add_new_book('Четыреста пятьдесят один градус по Фаренгейту')
 
-        assert len(book) == 0        
+        assert len(book.books_genre) == 0        
+
+    def test_add_new_book_add_book_without_genre(self, book):
+
+        book.add_new_book('Марсианин')
+
+        assert  book.books_genre['Марсианин'] == ''          
+
+    def test_set_book_genre_add_genre(self, book):
+
+        book.books_genre['Марсианин']=''
+        book.set_book_genre('Марсианин','Фантастика')
+
+        assert  book.books_genre['Марсианин'] == 'Фантастика'          
+
+    def test_set_book_genre_neggative_add_genre_for_missing_book(self, book):
+
+        book.set_book_genre('Марсианин','Боевик')
+
+        assert  len(book.books_genre) == 0  
+
+    def test_set_book_genre_neggative_add_unknown_genre(self, book):
+
+        book.books_genre['Марсианин']=''
+        book.set_book_genre('Марсианин','Боевик')
+
+        assert  book.books_genre['Марсианин'] == ''     
+
+
+
