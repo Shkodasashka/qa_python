991f858641b9a30b512438c81d00b634bb564305
Sasha Shkoda <shkoda_sashka@mail.ru>
2025-09-27 23:32:22 +0300

    Добавлены негативные тесты на повторное добавление той же книги и добавление книги с названием за диапазоном 0-40

--- test_main.py
+++ test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -17,7 +18,21 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(book.get_books_rating()) == 2
+        assert len(book) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    
+    def test_add_new_book_neggative_add_same_book(self, book):
+
+        book.add_new_book('Марсианин')
+        book.add_new_book('Марсианин')
+
+        assert len(book) == 1
+
+    def test_add_new_book_neggative_add_book_out_of_range(self, book):
+
+        book.add_new_book('')
+        book.add_new_book('Четыреста пятьдесят один градус по Фаренгейту')
+
+        assert len(book) == 0        
